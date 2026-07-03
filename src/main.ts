7ff06da9cbc64293440a72ecac01109fccdbe811
titan <titanio.yudista@cakrawala.ac.id>
2026-07-03 18:32:26 +0700

    chore: bind application to 0.0.0.0 to enable external access

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ async function bootstrap() {
 
   // 7. Start application
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}/api`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:${port}/api`);
 }
 bootstrap();
